2f5e34e7025f013127c559fec25ab961e78775b3
el mehdi  iggouch <elmehdi.iggouch@gmail.com>
2026-09-06 18:44:33 +0100

    .

--- ETL/EXTRACT.py
+++ ETL/EXTRACT.py
@@ -1,34 +1,31 @@
 
-# creating class  Extraction , to extarct data from the source and pass it to the next step of 
-# the pipeline 
-
-from logging import log
-from pathlib import Path
 import pandas as pd
+from pathlib import Path
+
 
 
 class Extraction:
-    def __init__(self, source_path: str):
+    def __init__(self, source_path):
         self.source_path = source_path
 
     def extract_data(self) -> pd.DataFrame:
-        """
-        Extracts data from the source path and returns it as a pandas DataFrame.
-
-        Returns:
-            pd.DataFrame: The extracted data.
-        """
+        
         try:
             # Check if the source path exists
-            if not Path.Path(self.source_path).exists():
-                log.error(f"Source path {self.source_path} does not exist.")
+            if not Path(self.source_path).exists():
+                
                 raise FileNotFoundError(f"Source path {self.source_path} does not exist.")
 
             # Read the data into a DataFrame
             data = pd.read_csv(self.source_path)
-            log.info(f"Data extracted successfully from {self.source_path}.")
+            
             return data
 
         except Exception as e:
-            log.error(f"An error occurred while extracting data: {e}")
+            
             raise
+
+
+
+
+
---
+++ ETL/kika.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from EXTRACT import Extraction
+from pathlib import Path
+
+
+
+
+
+jjj = "Data Inspection: start"
+
+for i in range(0,20):
+    print(f"========================================{jjj}")
+
+
+
+
+def printer(data ):
+
+     case = {}
+     case.add(data)
+     
